ad836633f2486a3d0e8b69e49aecd004e603ae59
Maximilian Goisser <goisser94@gmail.com>
2019-03-08 21:52:07 +0100

    Add deserialization

--- src/encoding.rs
+++ src/encoding.rs
@@ -1,13 +1,12 @@
-use rustc_hex::ToHex;
-use serde::ser::SerializeSeq;
-use serde::ser::SerializeStruct;
-use serde::Serialize;
-use serde::Serializer;
+use rustc_hex::{FromHex, ToHex};
+use serde::de::{self, Deserialize, Deserializer, MapAccess, Visitor};
+use serde::ser::{Serialize, SerializeSeq, SerializeStruct, Serializer};
+use trie_db::NibbleSlice;
 
 use crate::types::EntryOwned;
 use crate::types::NodeDiffOwned;
 
-struct PrefixedFormat<T: SerializePrefixedFormat>(T);
+struct PrefixedFormat<T>(T);
 
 pub trait SerializePrefixedFormat {
     fn serialize_prefixed_format<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
@@ -46,14 +45,23 @@ impl<T: SerializePrefixedFormat> SerializePrefixedFormat for Vec<T> {
     }
 }
 
+impl SerializePrefixedFormat for Vec<u8> {
+    fn serialize_prefixed_format<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(&format!("0x{}", self.to_hex::<String>()))
+    }
+}
+
 impl SerializePrefixedFormat for EntryOwned {
     fn serialize_prefixed_format<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
         let mut s = serializer.serialize_struct("EntryOwned", 2)?;
-        s.serialize_field("key", &format!("0x{}", self.key.inner.to_hex::<String>()))?;
-        s.serialize_field("value", &format!("0x{}", self.value.to_hex::<String>()))?;
+        s.serialize_field("key", &PrefixedFormat(&self.key.inner))?;
+        s.serialize_field("value", &PrefixedFormat(&self.value))?;
         s.end()
     }
 }
@@ -71,15 +79,125 @@ impl SerializePrefixedFormat for NodeDiffOwned {
     }
 }
 
+pub trait DeserializePrefixedFormat<'de>: Sized {
+    fn deserialize_prefixed_format<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>;
+}
+
+impl<'de, T: DeserializePrefixedFormat<'de>> Deserialize<'de> for PrefixedFormat<T> {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        DeserializePrefixedFormat::deserialize_prefixed_format(deserializer)
+    }
+}
+
+impl<'de, T: DeserializePrefixedFormat<'de>> DeserializePrefixedFormat<'de> for PrefixedFormat<T> {
+    fn deserialize_prefixed_format<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        Ok(PrefixedFormat(
+            DeserializePrefixedFormat::deserialize_prefixed_format(deserializer)?,
+        ))
+    }
+}
+
+impl<'de> DeserializePrefixedFormat<'de> for Vec<u8> {
+    fn deserialize_prefixed_format<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct StringVisitor;
+
+        impl<'de> Visitor<'de> for StringVisitor {
+            type Value = Vec<u8>;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                write!(formatter, "a hex encoded string prefixed by 0x")
+            }
+
+            fn visit_str<E>(self, s: &str) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                if &s[0..2] != "0x" {
+                    return Err(de::Error::invalid_value(de::Unexpected::Str(s), &self));
+                }
+                Ok(s[2..].from_hex().map_err(de::Error::custom)?)
+            }
+        }
+
+        deserializer.deserialize_str(StringVisitor)
+    }
+}
+
+impl<'de> DeserializePrefixedFormat<'de> for EntryOwned {
+    fn deserialize_prefixed_format<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        struct StructVisitor;
+
+        impl<'de> Visitor<'de> for StructVisitor {
+            type Value = EntryOwned;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                write!(formatter, "struct EntryOwned")
+            }
+
+            fn visit_map<V>(self, mut map: V) -> Result<Self::Value, V::Error>
+            where
+                V: MapAccess<'de>,
+            {
+                let mut val_key = None;
+                let mut val_value = None;
+                while let Some(key) = map.next_key()? {
+                    match key {
+                        "key" => {
+                            if val_key.is_some() {
+                                return Err(de::Error::duplicate_field("key"));
+                            }
+                            let inner_val: PrefixedFormat<Vec<u8>> = map.next_value()?;
+                            let nibble = NibbleSlice::new(&inner_val.0);
+                            val_key = Some(nibble.into());
+                        }
+                        "value" => {
+                            if val_value.is_some() {
+                                return Err(de::Error::duplicate_field("value"));
+                            }
+                            let inner_val: PrefixedFormat<Vec<u8>> = map.next_value()?;
+                            val_value = Some(inner_val.0);
+                        }
+                        key => return Err(de::Error::unknown_field(key, FIELDS)),
+                    }
+                }
+                let val_key = val_key.ok_or_else(|| de::Error::missing_field("key"))?;
+                let val_value = val_value.ok_or_else(|| de::Error::missing_field("value"))?;
+                Ok(EntryOwned {
+                    key: val_key,
+                    value: val_value,
+                })
+            }
+        }
+
+        const FIELDS: &'static [&'static str] = &["key", "value"];
+        deserializer.deserialize_struct("Duration", FIELDS, StructVisitor)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::PrefixedFormat;
+    use crate::types::EntryOwned;
     use rustc_hex::FromHex;
     use serde_json::Value as JsonValue;
 
     #[test]
     fn json_serialize_prefixed_format_entry_owned() {
-        let entry = PrefixedFormat(crate::types::EntryOwned {
+        let entry = PrefixedFormat(EntryOwned {
             key: crate::types::NibbleOwned { inner: vec![] },
             value: b"baz".to_vec(),
         });
@@ -96,5 +214,30 @@ mod tests {
             .from_hex()
             .unwrap();
         assert_eq!("baz", std::str::from_utf8(&baz).unwrap())
+    }
+
+    #[test]
+    fn json_deserialize_prefixed_format_vec_u8() {
+        let expected = vec![0, 15, 16];
+        let data = r#""0x000f10""#;
+        let deserialized: PrefixedFormat<Vec<u8>> = serde_json::from_str(data).unwrap();
+
+        assert_eq!(expected, deserialized.0);
+    }
+
+    #[test]
+    fn json_deserialize_prefixed_format_entry_owned() {
+        let expected = EntryOwned {
+            key: crate::types::NibbleOwned { inner: vec![] },
+            value: b"baz".to_vec(),
+        };
+        let data = r#"
+            {
+                "key": "0x",
+                "value": "0x62617a"
+            }"#;
+        let deserialized: PrefixedFormat<EntryOwned> = serde_json::from_str(data).unwrap();
+
+        assert_eq!(expected, deserialized.0);
     }
 }
